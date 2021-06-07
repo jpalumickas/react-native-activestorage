09efc99d3d9606a3dbcc96d99ca671ef82fc49e3
Justas Palumickas <jpalumickas@gmail.com>
2021-06-07 13:31:50 +0300

    Migrate some files to TypeScript

--- src/lib/insertOrReplace.ts
+++ src/lib/insertOrReplace.ts
@@ -1,4 +1,6 @@
-const insertOrReplace = (list, obj, key = 'id') => {
+type Item = { [key: string]: any };
+
+const insertOrReplace = (list: Item[], obj: Item, key = 'id') => {
   const newList = [...list];
   const index = newList.findIndex((it) => it[key] === obj[key]);
 
